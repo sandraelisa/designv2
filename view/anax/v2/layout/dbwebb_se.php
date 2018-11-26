f9bf72d0cc3a4a5bbd6aaee3ebc3256512ba0e5c
Sandra Vasarainen <sandravasarainen@hotmail.com>
2018-11-26 22:08:23 +0000

    implementing the vertical grid

--- view/anax/v2/layout/dbwebb_se.php
+++ view/anax/v2/layout/dbwebb_se.php
@@ -63,7 +63,6 @@ $route = "route-" . str_replace("/", "-", $di->get("request")->getRoute());
     <title><?= $title ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet">
-    <link rel="stylesheet" href="normalize.min.css" />
 
     <?php if (isset($favicon)) : ?>
     <link rel="icon" href="<?= asset($favicon) ?>">
